8312e1d7bb5ae943f0c0c0e80b7f36b96cd3122b
tcc545-art <tcc545@york.ac.uk>
2025-10-31 17:52:34 +0000

    week5_1

---
+++ exercise1-1/sketch.js
@@ -0,0 +1,22 @@
+let diameters = [360,280,200,150];
+let colourVals = [60,140,180,255];
+
+function setup() {
+    createCanvas(400, 400);
+    colorMode(RGB);
+}
+
+function draw() {
+    background(255);
+    
+    for (let i = 0; i < diameters.length; i++) {
+        fill (0,0,colourVals[i]);
+        ellipse(width/2, height/2, diameters[i]);
+    }
+}
+
+function mouseClicked() {
+   
+    let lastColour = colourVals.pop();
+    colourVals.unshift(lastColour);
+}
--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -1,9 +1,14 @@
-function setup() {
+let starX = [];
+let starY = [];
 
+function setup() {
+createCanvas(500,500);
 }
 
 function draw() {
-
+background(50,50,255);
+  updateStars();
+  drawAllStars();
 }
 
 /**
@@ -19,4 +24,20 @@ function star(x, y) {
     triangle(x + 50, y - 20, x, y - 20, x, y + 10);
     triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
     triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
+}
+function drawAllStars() {
+  for (let i = 0; i < starX.length; i++) {
+    star(starX[i], starY[i]);
+  }
+}
+
+function updateStars() {
+  for (let i = 0; i < starY.length; i++) {
+    starY[i] += 2; 
+  }
+}
+function mouseClicked() {
+  starX.push(mouseX);
+  starY.push(mouseY);
+  return false; 
 }
